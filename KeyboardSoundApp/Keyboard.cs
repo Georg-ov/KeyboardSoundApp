7bb71ed9d8e88074282c481a94d1353c177c1c65
Georg Usin <georgusin@hotmail.es>
2025-03-13 23:00:51 +0100

    Added a different sound for the Enter button

--- KeyboardSoundApp/Keyboard.cs
+++ KeyboardSoundApp/Keyboard.cs
@@ -31,7 +31,18 @@ namespace KeyboardSoundApp
             {
                 try
                 {
-                    using (SoundPlayer player = new SoundPlayer("C:\\Users\\Yoki\\source\\repos\\KeyboardSoundApp\\KeyboardSoundApp\\resources\\sounds\\tecla_1.wav")) // Ruta relativa o absoluta
+                    string soundFile;
+
+                    if (key == Keys.Enter)
+                    {
+                        soundFile = "C:\\Users\\Yoki\\source\\repos\\KeyboardSoundApp\\KeyboardSoundApp\\resources\\sounds\\enter_1.wav";
+                    }
+                    else
+                    {
+                        soundFile = "C:\\Users\\Yoki\\source\\repos\\KeyboardSoundApp\\KeyboardSoundApp\\resources\\sounds\\tecla_1.wav";
+                    }
+
+                    using (SoundPlayer player = new SoundPlayer(soundFile))
                     {
                         player.Play(); // Reproduce el sonido
                     }
@@ -42,64 +53,66 @@ namespace KeyboardSoundApp
                 }
             }
         }
-    }
 
-    public class LowLevelKeyboardHook
-    {
-        private const int WH_KEYBOARD_LL = 13;
-        private const int WM_KEYDOWN = 0x0100;
-        private IntPtr hookId = IntPtr.Zero;
-        private HookProc hookCallback;
 
-        public event Action<Keys> OnKeyPressed;
-
-        public LowLevelKeyboardHook()
+        public class LowLevelKeyboardHook
         {
-            hookCallback = HookCallback;
-        }
 
-        public void HookKeyboard()
-        {
-            hookId = SetHook(hookCallback);
-        }
+            private const int WH_KEYBOARD_LL = 13;
+            private const int WM_KEYDOWN = 0x0100;
+            private IntPtr hookId = IntPtr.Zero;
+            private HookProc hookCallback;
 
-        public void UnhookKeyboard()
-        {
-            UnhookWindowsHookEx(hookId);
-        }
+            public event Action<Keys> OnKeyPressed;
 
-        private IntPtr SetHook(HookProc proc)
-        {
-            using (var curProcess = System.Diagnostics.Process.GetCurrentProcess())
-            using (var curModule = curProcess.MainModule)
+            public LowLevelKeyboardHook()
             {
-                return SetWindowsHookEx(WH_KEYBOARD_LL, proc, GetModuleHandle(curModule.ModuleName), 0);
+                hookCallback = HookCallback;
             }
-        }
 
-        private IntPtr HookCallback(int nCode, IntPtr wParam, IntPtr lParam)
-        {
-            if (nCode >= 0 && wParam == (IntPtr)WM_KEYDOWN)
+            public void HookKeyboard()
             {
-                int vkCode = Marshal.ReadInt32(lParam);
-                OnKeyPressed?.Invoke((Keys)vkCode);
+                hookId = SetHook(hookCallback);
             }
-            return CallNextHookEx(hookId, nCode, wParam, lParam);
+
+            public void UnhookKeyboard()
+            {
+                UnhookWindowsHookEx(hookId);
+            }
+
+            private IntPtr SetHook(HookProc proc)
+            {
+                using (var curProcess = System.Diagnostics.Process.GetCurrentProcess())
+                using (var curModule = curProcess.MainModule)
+                {
+                    return SetWindowsHookEx(WH_KEYBOARD_LL, proc, GetModuleHandle(curModule.ModuleName), 0);
+                }
+            }
+
+            private IntPtr HookCallback(int nCode, IntPtr wParam, IntPtr lParam)
+            {
+                if (nCode >= 0 && wParam == (IntPtr)WM_KEYDOWN)
+                {
+                    int vkCode = Marshal.ReadInt32(lParam);
+                    OnKeyPressed?.Invoke((Keys)vkCode);
+                }
+                return CallNextHookEx(hookId, nCode, wParam, lParam);
+            }
+
+            [DllImport("user32.dll")]
+            private static extern IntPtr SetWindowsHookEx(int idHook, HookProc lpfn, IntPtr hMod, uint dwThreadId);
+
+            [DllImport("user32.dll")]
+            [return: MarshalAs(UnmanagedType.Bool)]
+            private static extern bool UnhookWindowsHookEx(IntPtr hhk);
+
+            [DllImport("user32.dll")]
+            private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode, IntPtr wParam, IntPtr lParam);
+
+            [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+            private static extern IntPtr GetModuleHandle(string lpModuleName);
+
+            private delegate IntPtr HookProc(int nCode, IntPtr wParam, IntPtr lParam);
         }
-
-        [DllImport("user32.dll")]
-        private static extern IntPtr SetWindowsHookEx(int idHook, HookProc lpfn, IntPtr hMod, uint dwThreadId);
-
-        [DllImport("user32.dll")]
-        [return: MarshalAs(UnmanagedType.Bool)]
-        private static extern bool UnhookWindowsHookEx(IntPtr hhk);
-
-        [DllImport("user32.dll")]
-        private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode, IntPtr wParam, IntPtr lParam);
-
-        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
-        private static extern IntPtr GetModuleHandle(string lpModuleName);
-
-        private delegate IntPtr HookProc(int nCode, IntPtr wParam, IntPtr lParam);
     }
 }
